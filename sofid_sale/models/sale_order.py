d0d087144de6142d49fea9dd607b06270f7f9717
Lids <42377939+lids11@users.noreply.github.com>
2025-02-18 09:44:16 +0300

    [fix] : sale

--- sofid_sale/models/sale_order.py
+++ sofid_sale/models/sale_order.py
@@ -7,4 +7,13 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
     
     purchase_order_ids = fields.Many2many('purchase.order')
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.constrains('product_uom_qty')
+    def _check_product_uom_qty(self):
+        for record in self:
+            if not isinstance(record.product_uom_qty, int) or record.product_uom_qty != int(record.product_uom_qty):
+                raise ValidationError("Le champ doit être un entier.")
    
